8b2368cc2aa87015a70eed86bbee103ff02e8c11
StefanoM98 <smartino030@gmail.com>
2023-03-10 14:42:32 +0100

    Add: fixed

--- js/script.js
+++ js/script.js
@@ -2,23 +2,19 @@ const gridElem = document.querySelector (".grid")
 
 for (let i = 1; i <= 100; i++) {
 
-    let divisibile;
     let color;
     let text;
 
     if (i % 15 === 0) {
         console.log ("FizzBuzz")
-        divisibile = "divisibileper15"
         color = "fizzbuzz"
         text = "FizzBuzz"
     } else if (i % 5 === 0){
         console.log ("Buzz")
-        divisibile = "divisibileper5"
         color = "buzz"
         text = "Buzz"
     } else if (i % 3 === 0) {
         console.log("Fizz");
-        divisibile = "divisibileper3"
         color = "fizz"
         text = "Fizz"
     } else {
