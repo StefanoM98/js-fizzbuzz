315ca4048ca097959559fb30045ff4379ceb849f
StefanoM98 <smartino030@gmail.com>
2023-03-10 14:21:47 +0100

    Add: save gridElem

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const gridElem = document.querySelector (".grid")
+for (let i = 1; i <= 100; i++) {
+
+    let oddEven;
+    let color;
+
+    if (i % 15 === 0 ) {
+        console.log ("FizzBuzz")
+        oddEven = "divisibileper15"
+        color = ".fizzbuzz"
+    }
+}
