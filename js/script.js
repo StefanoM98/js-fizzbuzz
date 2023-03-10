647d1853fd14060c143d01968cffc66cf8380e27
StefanoM98 <smartino030@gmail.com>
2023-03-10 14:31:11 +0100

    Add: other else if and print

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 const gridElem = document.querySelector (".grid")
 for (let i = 1; i <= 100; i++) {
 
-    let oddEven;
+    let divisibile;
     let color;
 
-    if (i % 15 === 0 ) {
+    if (i % 15 === 0) {
         console.log ("FizzBuzz")
-        oddEven = "divisibileper15"
-        color = ".fizzbuzz"
+        divisibile = "divisibileper15"
+        color = "fizzbuzz"
+    } else if (i % 5 === 0){
+        console.log ("Buzz")
+        divisibile = "divisibileper5"
+        color = "buzz"
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+        divisibile = "divisibileper3"
+        color = "fizz"
     }
+
+    gridElem.innerHTML += `<div class="box ${color}">${i}</div>`
 }
